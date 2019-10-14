3a832ce8cb537324c8a0dcddc65d9acb32b12b3d
Kyle Hilton <exlexer@Kyles-MacBook-Pro.local>
2019-10-14 12:09:52 -0600

    add security to db connection

--- api/lib/db.js
+++ api/lib/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://postgres@localhost:5432/contacts',
+  connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production',
 });
 
