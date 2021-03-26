55cab9d95f0a78d632f3dd131c67bbc288f5ab79
Kyle Hilton <kylehilton92@gmail.com>
2021-03-26 15:36:39 -0600

    more logging

--- api/lib/db.ts
+++ api/lib/db.ts
@@ -1,13 +1,15 @@
 import { Client } from 'pg';
-import type { QueryResult } from 'pg';
+import { QueryResult } from 'pg';
+
+console.log(process.env.NODE_ENV);
 
 const client: Client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === 'production'
       ? {
-        rejectUnauthorized: false,
-      }
+          rejectUnauthorized: false,
+        }
       : false,
 });
 
@@ -17,12 +19,15 @@ client.on('error', (err: Error) => {
   throw err;
 });
 
-export const query = (text: string, params?: any[]): Promise<any[]> => new Promise((resolve, reject): void => {
-  client.query(text, params, (error: Error, res: QueryResult) => {
-    if (error) return reject(error);
+export const query = (text: string, params?: any[]): Promise<any[]> =>
+  new Promise(
+    (resolve, reject): void => {
+      client.query(text, params, (error: Error, res: QueryResult) => {
+        if (error) return reject(error);
 
-    if (res.command === 'DELETE' && res.rowCount < 1) reject(new Error('no rows deleted'));
-    if (res.command === 'INSERT' && res.rowCount < 1) reject(new Error('no rows inserted'));
-    if (['INSERT', 'SELECT'].includes(res.command)) resolve(res.rows);
-  });
-});
+        if (res.command === 'DELETE' && res.rowCount < 1) reject(new Error('no rows deleted'));
+        if (res.command === 'INSERT' && res.rowCount < 1) reject(new Error('no rows inserted'));
+        if (['INSERT', 'SELECT'].includes(res.command)) resolve(res.rows);
+      });
+    },
+  );
