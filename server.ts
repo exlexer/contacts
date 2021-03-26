3bcc4f72c607c9f3eae27988d479ac7df76b0c06
Kyle Hilton <kylehilton92@gmail.com>
2021-03-26 15:57:17 -0600

    client test

--- server.ts
+++ server.ts
@@ -15,6 +15,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('client/dist'));
 app.use('/api', router);
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (const row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 app.use((err, req, res, next) => {
   console.log(err);
   if (res.headersSent) {
