db705bec432dac590e3ccf3bf36eec46745c20a3
Kyle Hilton <kylehilton92@gmail.com>
2021-03-26 10:28:49 -0600

    removes pooling

--- api/lib/db.js
+++ api/lib/db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+const client = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === 'production'
@@ -10,31 +10,30 @@ const pool = new Pool({
       : false,
 });
 
-pool.on('error', (err) => {
+client.on('error', (err) => {
   console.error('idle client error', err.message, err.stack);
 });
 
 module.exports = {
   query(text, params) {
     return new Promise((resolve, reject) => {
-      pool.connect((err, client, done) => {
-        if (err) return reject(err);
-        try {
-          client.query(text, params, (error, res) => {
-            if (error) return reject(error);
+      client.connect();
 
-            if (res.command === 'DELETE' && res.rowCount < 1) reject(new Error('no rows deleted'));
-            if (res.command === 'INSERT' && res.rowCount < 1) reject(new Error('no rows inserted'));
-            if (res.command === 'INSERT' && res.rows.length === 1) resolve(res.rows[0]);
-            if (res.command === 'INSERT' && res.rows.length > 1) resolve(res.rows);
-            if (res.command === 'SELECT' && res.rowCount === 1) resolve(res.rows[0]);
-            if (res.command === 'SELECT' && res.rowCount > 1) resolve(res.rows);
-            return resolve(res);
-          });
-        } finally {
-          client.release();
-        }
-      });
+      try {
+        client.query(text, params, (error, res) => {
+          if (error) return reject(error);
+
+          if (res.command === 'DELETE' && res.rowCount < 1) reject(new Error('no rows deleted'));
+          if (res.command === 'INSERT' && res.rowCount < 1) reject(new Error('no rows inserted'));
+          if (res.command === 'INSERT' && res.rows.length === 1) resolve(res.rows[0]);
+          if (res.command === 'INSERT' && res.rows.length > 1) resolve(res.rows);
+          if (res.command === 'SELECT' && res.rowCount === 1) resolve(res.rows[0]);
+          if (res.command === 'SELECT' && res.rowCount > 1) resolve(res.rows);
+          return resolve(res);
+        });
+      } finally {
+        client.end();
+      }
     });
   },
 };
